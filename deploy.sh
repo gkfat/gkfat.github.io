785848c90db781418ca92008693400186bac61ff
gkfat <gkgkdesign@gmail.com>
2023-02-09 11:19:54 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
-#!/usr/bin/env sh
+#/!bin/bash
 
-# 若出現錯誤就停止
-set -e
+TODAY=$(date +"%Y%m%d")
 
 hexo g d
-
-# cd 
 cd public
-git init
-git add -A
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/gkfat/gkfat.github.io.git master:gh-pages
-
-cd -
+git add .
+git commit -m "New gh-pages release $TODAY"
+git push
+cd ..
